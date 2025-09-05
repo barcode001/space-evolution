9520a83883b9a954c7139d53b26f56e421fc2392
Samuel Flores <samuelflores@Samuels-Mac-mini.local>
2025-09-05 12:49:27 -0500

    build all pages and fixed bug

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -4,9 +4,7 @@ function ThankYou() {
   return (
     <div>
       <h1>Thank You!</h1>
-      <p className="message1">
-        We have received your message and will get back to you shortly.
-      </p>
+      <p>We have received your message and will get back to you shortly.</p>
     </div>
   );
 }
