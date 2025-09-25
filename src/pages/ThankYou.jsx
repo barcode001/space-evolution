81f9dc412c066c5891be4a5d1856306c77020a64
Samuel Flores <samuelflores@Samuels-Mac-mini.local>
2025-09-25 05:46:37 -0500

    add it thankyou.jsx

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function ThankYou() {
+export default function ThankYou() {
   return (
-    <div>
-      <h1>Thank You!</h1>
-      <p className="thankyoums">
-        We have received your message and will get back to you shortly.
-      </p>
-    </div>
+    <section className="thankyou section">
+      <div className="container thankyou__wrap">
+        <h1 className="thankyou__title">Thank You!</h1>
+        <p className="thankyou__message">
+          We’ve received your message and will be in touch shortly. We
+          appreciate your interest in Space Evolution.
+        </p>
+
+        <div className="thankyou__actions">
+          <Link to="/" className="btn btn--primary">
+            Back to Home
+          </Link>
+          <a href="tel:13464791647" className="btn btn--ghost">
+            Call Us Now
+          </a>
+        </div>
+      </div>
+    </section>
   );
 }
-
-export default ThankYou;
