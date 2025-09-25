25fa95b809e19cd6e6f8c68b6f9b7c773fdfd0ae
Samuel Flores <samuelflores@Samuels-Mac-mini.local>
2025-09-25 08:02:06 -0500

    final webstie

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -50,9 +50,7 @@ export default function Contact() {
       formRef.current.reset();
     } catch (err) {
       console.error("EmailJS error:", err); // 👈 add this
-      console.log("✅ SERVICE_ID:", import.meta.env.VITE_EMAILJS_SERVICE_ID);
-      console.log("✅ TEMPLATE_ID:", import.meta.env.VITE_EMAILJS_TEMPLATE_ID);
-      console.log("✅ PUBLIC_KEY:", import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
+
       setStatus({
         type: "error",
         msg: "Something went wrong sending your message. Please call or email us.",
